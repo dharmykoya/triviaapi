e5791b02b8415add0a42eff61b47256838e9e96e
Damilola Adekoya <dharmykoya38@gmail.com>
2019-10-30 01:07:17 +0100

    bug(fix errors):fix error response To return error response on each endpoint To update readme

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -47,6 +47,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(data['categories'])
 
+    def test_404_if_category_does_not_exist(self):
+        response = self.client().get('/categories/4737/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
     def test_get_questions(self):
         response = self.client().get('/questions')
         data = json.loads(response.data)
@@ -56,12 +64,31 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data['categories']))
         self.assertTrue(len(data['questions']))
 
+    def test_404_if_page_does_not_exist(self):
+        response = self.client().get('/questions?page=200')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
     def test_create_question(self):
         response = self.client().post('/questions', json=self.new_question)
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['question_id'])
+
+    def test_422_if_info_supplied_is_empty(self):
+        wrong_question = {
+            'question': 'hello?',
+            'new_answer': 'dja'
+        }
+        response = self.client().post('/questions', json=wrong_question)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], "unprocessable")
 
     def test_get_questions_by_category(self):
         response = self.client().get('/categories/2/questions')
@@ -83,6 +110,15 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(data['current_category'])
 
+    def test_422_if_search_term_is_not_provided(self):
+        response = self.client().post('/questions/search',
+                                      json={'something': 'title'})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], "unprocessable")
+
     def test_delete_question(self):
         response = self.client().delete('/questions/6')
         data = json.loads(response.data)
@@ -90,6 +126,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(data['deleted_id'], 6)
+
+    def test_404_if_question_not_found_on_delete(self):
+        response = self.client().delete('/questions/68573')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
 
     def test_play_quiz(self):
         body = {
